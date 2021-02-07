820dd2cf14b9554367203a7357484717c1c5a534
abhisheksingh98 <writetoabhishek07@gmail.com>
2021-02-07 22:41:30 +0530

    solve deep copy object

--- js-exercises/deep-copy-object/deepCopyObject.js
+++ js-exercises/deep-copy-object/deepCopyObject.js
@@ -1,13 +1,11 @@
 const deepCopyObject = objToCopy => {
 
-    if (typeof objToCopy !== 'object' || objToCopy === null) return objToCopy;
-    
+    if (typeof objToCopy !== 'object' || objToCopy === null) return objToCopy;    
     const resultantObject = Array.isArray(objToCopy) ? [] : {};
     
     for (const name of Object.getOwnPropertyNames(objToCopy)) {
         resultantObject[name] = deepCopyObject(objToCopy[name]);
-    }
-    
+    }    
     for (const symbol of Object.getOwnPropertySymbols(objToCopy)) {
         resultantObject[symbol] = deepCopyObject(objToCopy[symbol]);
     }
